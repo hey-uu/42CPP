ec025993657789e4053d94ac434b8140219d6e02
hey-uu <khy2714@gmail.com>
2023-12-22 02:31:31 +0900

    New(cpp05/ex02): Bureaucrat 작성 완료

---
+++ cpp05/ex02/Bureaucrat.cpp
@@ -0,0 +1,136 @@
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+#include <iomanip>
+
+void Bureaucrat::checkGradeInRange() const
+{
+    if (_grade > 150)
+        throw GradeTooLowException(_name);
+    else if (_grade < 1)
+        throw GradeTooHighException(_name);
+}
+
+// essenstial
+Bureaucrat::Bureaucrat() : _name("none"), _grade(150) {}
+
+Bureaucrat::Bureaucrat(std::string const& name, int grade)
+    : _name(name), _grade(grade)
+{
+    try {
+        checkGradeInRange();
+    } catch (std::exception const& e) {
+        std::cerr << "construction failed because " << e.what() << std::endl;
+        throw;
+    }
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const& other)
+    : _name(other._name), _grade(other._grade)
+{
+}
+
+Bureaucrat& Bureaucrat::operator=(Bureaucrat const& other)
+{
+    if (this == &other)
+        return *this;
+    *(const_cast<std::string*>(&_name)) = other._name;
+    _grade = other._grade;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+std::string const& Bureaucrat::getName() const
+{
+    return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return _grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+    try {
+        _grade--;
+        checkGradeInRange();
+    } catch (std::exception const& e) {
+        _grade++;
+        std::cerr << "failed to increment grade because " << e.what()
+                  << std::endl;
+    }
+}
+
+void Bureaucrat::decrementGrade()
+{
+    try {
+        _grade++;
+        checkGradeInRange();
+    } catch (std::exception const& e) {
+        _grade--;
+        std::cerr << "failed to decrement grade because " << e.what()
+                  << std::endl;
+    }
+}
+
+void Bureaucrat::signForm(AForm& form)
+{
+    try {
+        form.beSigned(*this);
+        std::cout << _name << " signed " << form.getName() << std::endl;
+    } catch (std::exception const& e) {
+        std::cout << _name << " couldn't sign " << form.getName() << " because "
+                  << e.what() << std::endl;
+    }
+}
+
+void Bureaucrat::executeForm(AForm const& form) const
+{
+    try {
+        form.execute(*this);
+        std::cout << _name << " executed " << form.getName() << std::endl;
+
+    } catch (std::exception const& e) {
+        std::cerr << _name << " failed to execute " << form.getName()
+                  << " because " << e.what() << std::endl;
+    }
+}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException(
+    std::string const& name) throw()
+    : _errmsg(name + "'s grade is too high")
+{
+}
+
+Bureaucrat::GradeTooHighException::~GradeTooHighException() throw() {}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return _errmsg.c_str();
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(
+    std::string const& name) throw()
+    : _errmsg(name + "'s grade is too low")
+{
+}
+
+Bureaucrat::GradeTooLowException::~GradeTooLowException() throw() {}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return _errmsg.c_str();
+}
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat const& bureau)
+{
+    os << "[ INFO  ] " << std::setw(10) << std::left << "type"
+       << " | " << std::setw(22) << std::left << "name"
+       << " | " << std::setw(10) << std::left << "grade"
+       << " | " << std::endl;
+    os << "          " << std::setw(10) << std::left << "Bureaucrat"
+       << " | " << std::setw(22) << std::left << bureau.getName() << " | "
+       << std::setw(10) << std::left << bureau.getGrade() << " | " << std::endl;
+    return os;
+}
---
+++ cpp05/ex02/Bureaucrat.hpp
@@ -0,0 +1,58 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <stdexcept>
+
+class AForm;
+
+class Bureaucrat
+{
+private:
+    std::string const _name;
+    int               _grade;
+    int static const _kLowestGrade = 150;
+    int static const _kHighestGrade = 1;
+
+    void checkGradeInRange() const;
+
+    class GradeTooHighException : public std::exception
+    {
+    private:
+        std::string const _errmsg;
+
+    public:
+        GradeTooHighException(std::string const& name) throw();
+        ~GradeTooHighException() throw();
+        const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+    private:
+        std::string const _errmsg;
+
+    public:
+        GradeTooLowException(std::string const& bureau) throw();
+        ~GradeTooLowException() throw();
+        const char* what() const throw();
+    };
+
+public:
+    Bureaucrat();
+    Bureaucrat(std::string const& name, int grade);
+    Bureaucrat(Bureaucrat const& other);
+    Bureaucrat& operator=(Bureaucrat const& other);
+    ~Bureaucrat();
+
+    std::string const& getName() const;
+    int                getGrade() const;
+    void               incrementGrade();
+    void               decrementGrade();
+    void               signForm(AForm &form);
+    void               executeForm(AForm const& form) const;
+};
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat const& bureau);
+
+#endif
