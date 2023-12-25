6bc18b86d81726a1439f93d80158fb742f410ff3
hey-uu <khy2714@gmail.com>
2023-12-26 00:37:23 +0900

    Fix(cpp08/ex00): const container도 가능하도록 수정

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -15,4 +15,15 @@ typename T::iterator easyfind(T& container, int target)
     return it;
 }
 
+template <typename T>
+typename T::const_iterator easyfind(const T& container, int target)
+{
+    typename T::const_iterator it
+        = std::find(container.begin(), container.end(), target);
+
+    if (it == container.end())
+        throw std::out_of_range("Element not found");
+    return it;
+}
+
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -4,15 +4,25 @@
 
 int main(void)
 {
-    int              myints[] = {1, 2, 3, 4, 5};
-    std::vector<int> myvector(myints, myints + sizeof(myints) / sizeof(int));
+    int              ints[] = {1, 2, 3, 4, 5};
+    std::vector<int> intvec(ints, ints + sizeof(ints) / sizeof(int));
+    const std::vector<int> intvec2(ints, ints + sizeof(ints) / sizeof(int));
 
     try {
-        std::vector<int>::iterator it = easyfind(myvector, 3);
+        ints[2] = 0;
+        std::vector<int>::iterator it = easyfind(intvec, 3);
         std::cout << *it << std::endl;
         *it = 0;
-        easyfind(myvector, 3);
+        easyfind(intvec, 3);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
 
+    try {
+        std::vector<int>::const_iterator it1 = easyfind(intvec2, 3);
+        std::cout << *it1 << std::endl;
+        std::vector<int>::const_iterator it2 = easyfind(intvec2, 9);
+        std::cout << *it2 << std::endl;
     } catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
