3678e417d3eb291f3f8b5a7331e5f0ac0c306c49
Hyeyun Kim <hyeyukim@c5r6s2.42seoul.kr>
2023-12-31 13:11:31 +0900

    Fix(cpp07/ex01): test case 추가

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -25,11 +25,36 @@ void test_const_int_array()
     // iter(arr, 3, increaseElement<int>);
 }
 
+void test_char_array()
+{
+    int arr[3] = {'a', 'b', 'c'};
+
+    std::cout << "====== test 3 : char array ======= " << std::endl;
+    std::cout << "> print value" << std::endl;
+    iter(arr, 3, printElement<int>);
+    std::cout << "> increase value " << std::endl;
+    iter(arr, 3, increaseElement<int>);
+    std::cout << "> print value" << std::endl;
+    iter(arr, 3, printElement<int>);
+}
+
+void test_const_char_array()
+{
+    const int arr[3] = {'a', 'b', 'c'};
+
+    std::cout << "====== test 4 : const char array ======= " << std::endl;
+    std::cout << "> print value" << std::endl;
+    iter(arr, 3, printElement<int>);
+    // compile error
+    // std::cout << "> increase value " << std::endl;
+    // iter(arr, 3, increaseElement<int>);
+}
+
 void test_string_array()
 {
     std::string arr[3] = {"hi", "hello", "bye"};
 
-    std::cout << "====== test 3 : string array ======= " << std::endl;
+    std::cout << "====== test 5 : string array ======= " << std::endl;
     std::cout << "> print value" << std::endl;
     iter(arr, 3, printElement<std::string>);
 }
@@ -38,7 +63,7 @@ void test_const_string_array()
 {
     const std::string arr[3] = {"hi", "hello", "bye"};
 
-    std::cout << "====== test 4 : const string array ======= " << std::endl;
+    std::cout << "====== test 6 : const string array ======= " << std::endl;
     std::cout << "> print value" << std::endl;
     iter(arr, 3, printElement<const std::string>);
 }
@@ -47,6 +72,8 @@ int main(void)
 {
     test_int_array();
     test_const_int_array();
+    test_char_array();
+    test_const_char_array();
     test_string_array();
     test_const_string_array();
 }
